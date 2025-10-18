cb8f56dee3ec4ae282b5de8e0254ec77663d57ed
SupermaxiMarket <monsunrise@gmail.com>
2025-10-18 17:46:53 +0200

    Feat: Affiche le composant principal du carrousel

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import VideoPlayer from './components/VideoPlayer.jsx';
+import CarrouselNutritionFrance from './components/CarrouselNutritionFrance.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <VideoPlayer />
+    <CarrouselNutritionFrance />
   </React.StrictMode>
 );
